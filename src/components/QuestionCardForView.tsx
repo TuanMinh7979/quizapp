56b2e5313fc8b0f685f8cfa18d449a1a8f66ec42
TuanMinh7979 <minhtuanto179@gmail.com>
2024-05-29 04:33:52 +0700

    stable

--- src/components/QuestionCardForView.tsx
+++ src/components/QuestionCardForView.tsx
@@ -20,8 +20,15 @@ const QuestionCardForView = (props: PropsType) => {
 
     return (
         <div style={{ borderBottom: "1px dashed black", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", padding: "10px 0px 10px 0px" }}>
-            <h3 style={{ alignSelf: "start" }}><span style={{ color: "blue", fontWeight: "bold" }}>{props.order + 1}</span>
-                &nbsp; &nbsp;  <span style={{ color: "#6d6d75", fontSize: "10px" }}>ID:{props.id}</span>
+            <h3 style={{ width: "100%", display: "flex", justifyContent: "space-between" }}><span>
+                <span style={{ color: "blue", fontWeight: "bold" }}>{props.order + 1}</span>
+                &nbsp; &nbsp;  <span style={{ color: "#6d6d75", fontSize: "10px" }}>ID:{props.id}</span></span>
+
+                <div><button>Watch Solution</button>  &nbsp;&nbsp; {props.eAnsLbl == props.rightLbl ?
+                    <span style={{ padding: "2px", background: "blue", color: "white" }}>Done</span> : props.eAnsLbl == '' ?
+                        <span style={{ padding: "2px", background: "yellow", color: "white" }}>Miss</span> :
+                        <span style={{ padding: "2px", background: "orange", color: "white" }}>Wrong</span>}</div>
+
             </h3>
             <div style={{ margin: "0 auto" }}>
                 <img style={{ objectFit: "cover", width: "1000px", height: "300px" }} src="https://i.ex-cdn.com/giadinhmoi.vn/files/news/2022/07/07/de-thi-mon-toan-tot-nghiep-thpt-quoc-gia-2022-tat-ca-ma-de-day-du-nhat-164634.jpg" alt="" />
@@ -30,22 +37,22 @@ const QuestionCardForView = (props: PropsType) => {
                 <div style={{ flex: "1", display: "flex", justifyContent: 'flex-start' }}>
                     <label
 
-                        className={`ans-label ans-label-disabled  ${selectedAns == 'A' ? 'selected' : ''} ${props.rightLbl == 'A' ? 'correct-ans' : ''}`} >A</label> <input disabled className='ans-radio' type="radio" id={`${props.id}A`} name={props.id} value="A" />
+                        className={`ans-label ans-label-disabled  ${selectedAns == 'A' && props.rightLbl == 'A' ? 'exact-ans' : selectedAns == 'A' ? 'selected' : props.rightLbl == 'A' ? 'right-ans' : ''} `} >A</label> <input disabled className='ans-radio' type="radio" id={`${props.id}A`} name={props.id} value="A" />
 
 
                 </div>
                 <div style={{ flex: "1", display: "flex", justifyContent: 'flex-start' }}>
-                    <label className={`ans-label ans-label-disabled ${selectedAns == 'B' ? 'selected' : ''} ${props.rightLbl == 'B' ? 'correct-ans' : ''}`} >B</label> <input disabled className='ans-radio' type="radio" id={`${props.id}B`} name={props.id} value="B" />
+                    <label className={`ans-label ans-label-disabled ${selectedAns == 'B' && props.rightLbl == 'B' ? 'exact-ans' : selectedAns == 'B' ? 'selected' : props.rightLbl == 'B' ? 'right-ans' : ''}`} >B</label> <input disabled className='ans-radio' type="radio" id={`${props.id}B`} name={props.id} value="B" />
 
 
                 </div>
                 <div style={{ flex: "1", display: "flex", justifyContent: 'flex-start' }}>
-                    <label className={`ans-label ans-label-disabled ${selectedAns == 'C' ? 'selected' : ''} ${props.rightLbl == 'C' ? 'correct-ans' : ''}`} >C</label> <input disabled className='ans-radio' type="radio" id={`${props.id}C`} name={props.id} value="C" />
+                    <label className={`ans-label ans-label-disabled ${selectedAns == 'C' && props.rightLbl == 'C' ? 'exact-ans' : selectedAns == 'C' ? 'selected' : props.rightLbl == 'C' ? 'right-ans' : ''}`} >C</label> <input disabled className='ans-radio' type="radio" id={`${props.id}C`} name={props.id} value="C" />
 
 
                 </div>
                 <div style={{ flex: "1", display: "flex", justifyContent: 'flex-start' }}>
-                    <label className={`ans-label ans-label-disabled ${selectedAns == 'D' ? 'selected' : ''} ${props.rightLbl == 'D' ? 'correct-ans' : ''}`} >D</label> <input disabled className='ans-radio' type="radio" id={`${props.id}D`} name={props.id} value="D" />
+                    <label className={`ans-label ans-label-disabled ${selectedAns == 'D' && props.rightLbl == 'D' ? 'exact-ans' : selectedAns == 'D' ? 'selected' : props.rightLbl == 'D' ? 'right-ans' : ''}`} >D</label> <input disabled className='ans-radio' type="radio" id={`${props.id}D`} name={props.id} value="D" />
 
 
                 </div>

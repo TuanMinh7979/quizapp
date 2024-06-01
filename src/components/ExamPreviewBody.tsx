4fbf31de116c4acdee9916d997bd2ccc7be0b517
TuanMinh7979 <minhtuanto179@gmail.com>
2024-06-01 22:35:19 +0700

    refactor

--- src/components/ExamPreviewBody.tsx
+++ src/components/ExamPreviewBody.tsx
@@ -3,8 +3,14 @@ const ExamPreviewBody = (props: any) => {
     const { correctCnt, answerCnt, questionCnt, description } = props;
     return (
         <div className='exam-preview-body'>
-            <span style={{ color:"blue" }}>{description}</span>
-            <div><span >Answered : </span><span>{answerCnt}/{questionCnt}</span></div>
+            <div style={{
+                color: "black", height: "80px", fontSize: "0.8rem", whiteSpace: "wrap",
+
+
+
+            }}>
+                {description}</div>
+            <div><span style={{ color: "blue" }}>Answered : </span><span>{answerCnt}/{questionCnt}</span></div>
             <div><span style={{ color: "green" }}>Correct : {correctCnt}</span></div>
             <div><span style={{ color: "red" }}>Wrong : {answerCnt - correctCnt}</span></div>
         </div>
